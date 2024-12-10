0a23f0ab8e05d2e208d225742f671c28abd16dd0
Tiaftrn <tiafitriana2012@gmail.com>
2024-12-10 17:57:37 +0700

    test

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -31,7 +31,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => 'db_website',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
