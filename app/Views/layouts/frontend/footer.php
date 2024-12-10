835176989605cd34792988ff43577eea2b285f46
Nindia Oktapia <148666929+Viary17@users.noreply.github.com>
2024-12-10 19:39:31 +0700

    Update footer

---
+++ app/Views/layouts/frontend/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/footer.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/sb-admin-2.min.css">
+
+    <title>Universitas Subang</title>
+   
+</head>
+<div class="campus-info">
+    <div class="campus-card">
+        <h3>Kampus I</h3>
+        <img src="<?= base_url('foto/kampus1.png'); ?>" alt="Kampus I">
+
+        <p>Jl. R.A. Kartini No. 4 Subang, Kabupaten Subang, Jawa Barat, Indonesia</p>
+    </div>
+    <div class="campus-card">
+        <h3>Kampus II</h3>
+        <img src="<?= base_url('foto/kampus2.png'); ?>" alt="Kampus II">
+
+        <p>Jl. Arief Rahman Hakim No. 8, Cibogo, Kabupaten Subang, Jawa Barat, Indonesia</p>
+    </div>
+</div>
+
